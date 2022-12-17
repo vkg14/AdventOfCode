fb5c05fc41940030e605d3e9a659bbbf10126ad5
Varun Ganesan <varun.k.ganesan@gmail.com>
2022-12-17 12:15:07 -0500

    Fix off by 1

--- 2022_py/day17.py
+++ 2022_py/day17.py
@@ -35,12 +35,14 @@ class Shape(abc.ABC):
         if any((x - 1, y) in rocks or x == 0 for x, y in blocks):
             return False
         self.x -= 1
+        return True
 
     def move_right(self, rocks):
         blocks = self.get_blocks()
         if any((x + 1, y) in rocks or x == 6 for x, y in blocks):
             return False
         self.x += 1
+        return True
 
 
 class FlatLine(Shape):
@@ -92,28 +94,22 @@ class Square(Shape):
         return [(self.x + i, self.y + j) for i in range(2) for j in range(2)]
 
 
-def solve(filename, n=1000000000000):
+def solve(filename, part_two=True):
     jetstream = read_input(filename)
     shapes = [FlatLine, Cross, LShape, VerticalLine, Square]
-    shape_idx = 0
-    gas_idx = 0
-    max_y = -1
-    current_shape = shapes[shape_idx].make(max_y)
+    shape_idx, gas_idx, max_y = 0, 0, -1
+    n = 1000000000000 if part_two else 2022
+    jetstream_methods = {-1: Shape.move_left, 1: Shape.move_right}
     rocks = set()
     # New tracking for cycle detection
     peaks = [0] * 7
     states_seen = dict()
     max_heights = dict()
     while shape_idx < n:
+        current_shape = shapes[shape_idx % len(shapes)].make(max_y)
         still_falling = True
         while still_falling:
-            gas = jetstream[gas_idx % len(jetstream)]
-            if gas == 1:
-                current_shape.move_right(rocks)
-            elif gas == -1:
-                current_shape.move_left(rocks)
-            else:
-                raise Exception("Bad jet gas input!")
+            jetstream_methods[jetstream[gas_idx % len(jetstream)]](current_shape, rocks)
             gas_idx += 1
             still_falling = current_shape.move_down(rocks)
         # Adjust rock set and height max
@@ -121,7 +117,7 @@ def solve(filename, n=1000000000000):
         old_max = max_y
         max_y = max(max_y, *[y for x, y in new_rocks])
         rocks.update(new_rocks)
-        # Record peak shape
+        # Record peak shape relative to the tallest column
         max_heights[shape_idx] = max_y
         dy = max_y - old_max
         for i in range(len(peaks)):
@@ -129,25 +125,25 @@ def solve(filename, n=1000000000000):
         for x, y in new_rocks:
             peaks[x] = max(peaks[x], y - max_y)
         state = (shape_idx % len(shapes), gas_idx % len(jetstream), tuple(peaks))
-        if state in states_seen and n > 10 ** 5:
+        if state in states_seen and part_two:
             # Found cycle!
             n_prev_shape = states_seen[state]
             prev_max_h = max_heights[n_prev_shape]
             cycle_height_change = max_y - prev_max_h
             cycle_length = shape_idx - n_prev_shape
-            target = n - n_prev_shape
+            # Need to cycle simulate till shape_idx == n-1 (0-indexed)
+            target = n - n_prev_shape - 1
             cycles = target // cycle_length
             leftover_height_change = max_heights[n_prev_shape + (target % cycle_length)] - prev_max_h
-            return prev_max_h + (cycle_height_change * cycles) + leftover_height_change
+            return prev_max_h + (cycle_height_change * cycles) + leftover_height_change + 1
         states_seen[state] = shape_idx
         # Move to next shape
         shape_idx += 1
-        current_shape = shapes[shape_idx % len(shapes)].make(max_y)
     return max_y + 1
 
 
 if __name__ == '__main__':
-    print(solve("example17.txt", n=2022))
-    print(solve("input17.txt", n=2022))
+    print(solve("example17.txt", part_two=False))
+    print(solve("input17.txt", part_two=False))
     print(solve("example17.txt"))
     print(solve("input17.txt"))
